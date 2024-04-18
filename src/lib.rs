cbae402f8e3d6fd750ef97ceb411e5ae98749142
Yokinman <yokinmun@gmail.com>
2024-04-18 14:11:37 -0400

    - Unused `use chime::time::TimeRanges`.

--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,6 @@ use bevy_ecs::system::{IntoSystem, ReadOnlySystemParam, System, SystemParamItem}
 use bevy_ecs::world::{Mut, World};
 use bevy_time::Time;
 
-use chime::time::TimeRanges;
-
 /// Builder entry point for adding chime events to a [`World`].
 pub trait AddChimeEvent {
 	fn add_chime_events<P, M, A, F>(&mut self, events: ChimeEventBuilder<P, M, A, F>) -> &mut Self
